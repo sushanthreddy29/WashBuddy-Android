ab95ceab107f89f0e0c6c54818d4662a34fead3d
sushanthreddy29 <spapa3@unh.newhaven.edu>
2024-10-15 21:23:31 -0400

    Added more navigations to view and created more view pages

--- app/src/main/java/com/unh/washbuddy_android/ui/home/NewOrder1Fragment.kt
+++ app/src/main/java/com/unh/washbuddy_android/ui/home/NewOrder1Fragment.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.navigation.fragment.findNavController
 import com.unh.washbuddy_android.R
+import com.unh.washbuddy_android.databinding.FragmentHomeBinding
+
 
 class NewOrder1Fragment : Fragment() {
 
@@ -18,9 +22,12 @@ class NewOrder1Fragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
+        val callback = requireActivity().onBackPressedDispatcher.addCallback(this){
+            //TODO: pop up for are you sure you want to go back?
+            findNavController().navigate(NewOrder1FragmentDirections.actionNewOrder1FragmentToNavigationHome())
+        }
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
