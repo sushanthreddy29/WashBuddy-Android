6223c4485be8157f85430732b078c2193f2d5548
sushanthreddy29 <spapa3@unh.newhaven.edu>
2024-11-13 18:13:04 -0500

    Password reset done using email

--- app/src/main/java/com/unh/washbuddy_android/passwordreset.kt
+++ app/src/main/java/com/unh/washbuddy_android/passwordreset.kt
@@ -1,20 +1,51 @@
 package com.unh.washbuddy_android
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.unh.washbuddy_android.databinding.ActivityPasswordresetBinding
 
 class passwordreset : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPasswordresetBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_passwordreset)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityPasswordresetBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        val callback = this.onBackPressedDispatcher.addCallback(this){
+            val intent = Intent(this@passwordreset,signin::class.java)
+            startActivity(intent)
         }
+
+        binding.resetpassword.setOnClickListener {
+            val email = binding.email.text.toString().trim()
+            if(email != ""){
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d("PasswordReset", "Email sent.")
+                            val intent = Intent(this,signin::class.java)
+                            startActivity(intent)
+                        } else {
+                            Log.e("PasswordReset", "Error: ${task.exception?.message}")
+                        }
+                    }
+
+            }
+            else{
+                Toast.makeText(this, "Please enter your email", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
 }
