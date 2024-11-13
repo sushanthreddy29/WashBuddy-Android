3c9e2479e857e824bbd533dc5fb6cd9187f34394
sushanthreddy29 <spapa3@unh.newhaven.edu>
2024-11-13 18:00:50 -0500

    Accounts page Done
    did accounts page where we can edit our personal details

--- app/src/main/java/com/unh/washbuddy_android/ui/account/AccountFragment.kt
+++ app/src/main/java/com/unh/washbuddy_android/ui/account/AccountFragment.kt
@@ -1,13 +1,21 @@
 package com.unh.washbuddy_android.ui.account
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.unh.washbuddy_android.MainActivity
 import com.unh.washbuddy_android.databinding.FragmentAccountBinding
+import com.unh.washbuddy_android.signin
+import com.unh.washbuddy_android.usersignin
 
 class AccountFragment : Fragment() {
 
@@ -27,11 +35,79 @@ private var _binding: FragmentAccountBinding? = null
     _binding = FragmentAccountBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
-    val textView: TextView = binding.textNotifications
-    notificationsViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
+      val email = binding.email
+      val firstname = binding.firstname
+      val lastname = binding.lastname
+      val username = binding.username
+
+    email.isEnabled = false
+
+    email.setText(usersignin.email)
+    firstname.setText(usersignin.firstname)
+    lastname.setText(usersignin.lastname)
+    username.setText(usersignin.username)
+
+    binding.updateprofile.setOnClickListener {
+      val builder = AlertDialog.Builder(requireContext())
+      builder.setTitle("Confirm Update")
+      builder.setMessage("Are you sure you want to update your details?")
+
+      builder.setPositiveButton("Yes") { dialog, _ ->
+        val firstname = binding.firstname.text.toString()
+        val lastname = binding.lastname.text.toString()
+        val username = binding.username.text.toString()
+
+        updateuserprofile(firstname, lastname, username)
+
+        dialog.dismiss()
+        Toast.makeText(requireContext(),"Profile Updated",Toast.LENGTH_SHORT).show()
+      }
+      builder.setNegativeButton("No") { dialog, _ ->
+        dialog.cancel() // Close the dialog if user selects "No"
+      }
+
+      // Show the dialog
+      val alertDialog = builder.create()
+      alertDialog.show()
     }
+
+    binding.logout.setOnClickListener {
+      FirebaseAuth.getInstance().signOut()
+      usersignin.username = ""
+      usersignin.email = ""
+      usersignin.firstname = ""
+      usersignin.lastname = ""
+      usersignin.useruid = ""
+      usersignin.documentid = ""
+
+      val intent = Intent(requireContext(), signin::class.java)
+      startActivity(intent)
+    }
+
     return root
+  }
+
+  private fun updateuserprofile(firstname: String, lastname: String, username: String){
+
+    val userupdate = FirebaseFirestore.getInstance().collection("UserCredentials").document(usersignin.documentid)
+
+    val updates = mapOf(
+      "Firstname" to firstname,
+      "Lastname" to lastname,
+      "Username" to username
+    )
+    userupdate.update(updates)
+      .addOnSuccessListener {
+        usersignin.username = username
+        usersignin.firstname = firstname
+        usersignin.lastname = lastname
+
+        Toast.makeText(requireContext(),"Profile Updated",Toast.LENGTH_SHORT).show()
+      }
+      .addOnFailureListener {
+        Toast.makeText(requireContext(),"Failed to Update",Toast.LENGTH_SHORT).show()
+      }
+
   }
 
 override fun onDestroyView() {
