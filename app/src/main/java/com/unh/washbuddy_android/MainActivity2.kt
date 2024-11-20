300d41a2dc22c86416f284b67a3ae488cd5c3cb7
sushanthreddy29 <spapa3@unh.newhaven.edu>
2024-11-19 20:29:36 -0500

    Removed unnecessary imports

--- app/src/main/java/com/unh/washbuddy_android/MainActivity2.kt
+++ app/src/main/java/com/unh/washbuddy_android/MainActivity2.kt
@@ -6,12 +6,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
-import com.unh.washbuddy_android.databinding.ActivityMainBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.unh.washbuddy_android.AppData.email
