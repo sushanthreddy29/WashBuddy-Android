8abe10370d560fc5890bb113bd796b70903f532c
sushanthreddy29 <spapa3@unh.newhaven.edu>
2024-12-06 21:48:39 -0500

    Removed the question mark from home page

--- app/src/main/java/com/unh/washbuddy_android/ui/home/HomeFragment.kt
+++ app/src/main/java/com/unh/washbuddy_android/ui/home/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.unh.washbuddy_android.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
